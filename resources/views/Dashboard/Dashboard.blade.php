ebff42044637181c8ed718909f7e154d95405600
imas <imasnurdianto2002@gmail.com>
2023-03-17 00:56:23 +0700

    fix dashboard script

--- resources/views/Dashboard/Dashboard.blade.php
+++ resources/views/Dashboard/Dashboard.blade.php
@@ -131,11 +131,3 @@
         </section>
     </div>
 @endsection
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"
-    integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#dt-dashboard').DataTable();
-    });
-</script>
