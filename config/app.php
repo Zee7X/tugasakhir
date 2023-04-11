8fe484458b9563b4658cb93aa1e0ebb7c0de630b
indomieee <muhammadaldi.ma47@gmail.com>
2023-04-11 17:13:38 +0700

    Fixing Edit tidak bisa saat kurang dari tanggal mulai

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Yoeunes\Toastr\ToastrServiceProvider::class,
 
     ],
 
@@ -212,6 +213,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Toastr' => Yoeunes\Toastr\Facades\Toastr::class,
     ])->toArray(),
 
 ];
