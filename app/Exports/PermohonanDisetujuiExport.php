df85c52d1342a28358afe7f5a3eccc56c41be61a
nurdianto0410 <imasnurdianto2002@gmail.com>
2023-07-26 22:59:32 +0700

    first

--- app/Exports/PermohonanDisetujuiExport.php
+++ app/Exports/PermohonanDisetujuiExport.php
@@ -2,6 +2,8 @@
 
 namespace App\Exports;
 
+use DateTime;
+use DateInterval;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
@@ -16,78 +18,81 @@ class PermohonanDisetujuiExport implements FromView
         $this->year = $year;
     }
 
-    public function view(): View
+    // Helper function to count weekdays
+    function countWeekdays($startDate, $endDate)
     {
-        $months = [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        ];
+        $startDate = new DateTime($startDate);
+        $endDate = new DateTime($endDate);
+        $currentDate = clone $startDate;
+        $count = 0;
 
-        $results = [];
-        $total_rentang_hari = [];
+        while ($currentDate <= $endDate) {
+            $dayOfWeek = $currentDate->format('N');
 
-        foreach ($months as $month) {
-            $result = DB::table('users')
-                ->select('users.nip', DB::raw('SUM(DATEDIFF(permohonan_cuti.tgl_akhir, permohonan_cuti.tgl_mulai)) AS rentang_hari'))
-                ->join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
-                ->join('hak_cuti', 'users.id', '=', 'hak_cuti.user_id')
-                ->leftJoin('units', 'users.unit_id', '=', 'units.id')
-                ->where('permohonan_cuti.status', '=', 4)
-                ->whereYear('permohonan_cuti.tgl_mulai', $this->year)
-                ->whereRaw('MONTH(permohonan_cuti.tgl_mulai) = ?', [$month])
-                ->groupBy('users.nip')
-                ->get();
-
-                foreach ($result as $row) {
-                    $nip = $row->nip;
-                    $totalDays = $row->rentang_hari ?? 0;
-    
-                    if (!isset($results[$nip])) {
-                        $results[$nip] = [];
-                        $total_rentang_hari[$nip] = 0;
-                    }
-    
-                    // Custom function to count weekdays (Monday to Friday)
-                    $currentTotalDays = $this->countWeekdays($month, $totalDays);
-    
-                    $results[$nip][$month] = $currentTotalDays+1;
-                    $total_rentang_hari[$nip] += $currentTotalDays+1;
-                }
-        }
-        return view('permohonancuti.export_excel', [
-            'permohonan_disetujui' => User::join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
-                ->join('hak_cuti', 'users.id', '=', 'hak_cuti.user_id')
-                ->leftJoin('units', 'users.unit_id', '=', 'units.id')
-                ->where('permohonan_cuti.jenis_cuti_id', 4)
-                ->whereYear('permohonan_cuti.tgl_mulai', $this->year)
-                ->where('permohonan_cuti.status', 4)
-                ->orderBy('permohonan_cuti.updated_at', 'DESC')
-                ->select('users.name', 'users.nip', 'users.jabatan', 'units.name_unit', 'hak_cuti.hak_cuti')
-                ->groupBy('users.nip')
-                ->get(),
-            'results' => $results,
-            'months' => $months,
-            'total_rentang_hari' => $total_rentang_hari
-        ]);
-    }
-
-    private function countWeekdays($month, $totalDays)
-    {
-        $year = $this->year;
-
-        $weekdays = 0;
-        $day = 1;
-        while ($day <= $totalDays) {
-            $dateString = "$year-$month-$day";
-            $dayOfWeek = date('N', strtotime($dateString));
-
-            // Check if the day is Monday to Friday (1 to 5)
+            // Hanya menghitung hari Senin - Jumat (kode 1 hingga 5)
             if ($dayOfWeek >= 1 && $dayOfWeek <= 5) {
-                $weekdays++;
+                $count++;
             }
 
-            $day++;
+            $currentDate->add(new DateInterval('P1D'));
         }
 
-        return $weekdays;
+        return $count;
     }
+
+    public function view(): View
+{
+    $months = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    ];
+
+    $results = [];
+    $total_rentang_hari = [];
+
+    foreach ($months as $month) {
+        $result = DB::table('users')
+            ->select('users.nip', 'permohonan_cuti.tgl_mulai', 'permohonan_cuti.tgl_akhir') // Include tgl_mulai and tgl_akhir in the SELECT statement
+            ->addSelect(DB::raw('SUM(DATEDIFF(permohonan_cuti.tgl_akhir, permohonan_cuti.tgl_mulai)) AS rentang_hari'))
+            ->join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
+            ->join('hak_cuti', 'users.id', '=', 'hak_cuti.user_id')
+            ->leftJoin('units', 'users.unit_id', '=', 'units.id')
+            ->where('permohonan_cuti.status', '=', 4)
+            ->whereYear('permohonan_cuti.tgl_mulai', $this->year)
+            ->whereRaw('MONTH(permohonan_cuti.tgl_mulai) = ?', [$month])
+            ->groupBy('users.nip', 'permohonan_cuti.tgl_mulai', 'permohonan_cuti.tgl_akhir') // Group by tgl_mulai and tgl_akhir
+            ->get();
+
+        foreach ($result as $row) {
+            $nip = $row->nip;
+            $totalDays = $row->rentang_hari ?? 0;
+            $weekdaysCount = $this->countWeekdays($row->tgl_mulai, $row->tgl_akhir); // Calculate weekdays count for each row
+
+            if (!isset($results[$nip])) {
+                $results[$nip] = [];
+                $total_rentang_hari[$nip] = 0;
+            }
+
+            // Calculate the total days and weekdays for the current month
+            $results[$nip][$month] = $weekdaysCount;
+            $total_rentang_hari[$nip] += $weekdaysCount;
+        }
+    }
+
+    return view('permohonancuti.export_excel', [
+        'permohonan_disetujui' => User::join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
+            ->join('hak_cuti', 'users.id', '=', 'hak_cuti.user_id')
+            ->leftJoin('units', 'users.unit_id', '=', 'units.id')
+            ->where('permohonan_cuti.jenis_cuti_id', 4)
+            ->whereYear('permohonan_cuti.tgl_mulai', $this->year)
+            ->where('permohonan_cuti.status', 4)
+            ->orderBy('permohonan_cuti.updated_at', 'DESC')
+            ->select('users.name', 'users.nip', 'users.jabatan', 'units.name_unit', 'hak_cuti.hak_cuti')
+            ->groupBy('users.nip')
+            ->get(),
+        'results' => $results,
+        'months' => $months,
+        'total_rentang_hari' => $total_rentang_hari
+    ]);
+}
+
 }
